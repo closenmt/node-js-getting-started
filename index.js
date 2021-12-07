311110f1a78ec02c7f87a2bae55efe8a4d3c430d
closenmt <closenmt@gmail.com>
2021-12-07 13:51:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const httpProxy=require('http-proxy')
-httpProxy.createServer({
+server=httpProxy.createServer({
   target: {
-    host: 'https://ooxxz.oschina.link',
+    host: 'ooxxz.oschina.link',
     port: 443
   },
   ssl: {
     key: fs.readFileSync('valid-ssl-key.pem', 'utf8'),
     cert: fs.readFileSync('valid-ssl-cert.pem', 'utf8')
   }
-}).listen(PORT);
+})
+server.listen(PORT);
+console.log("server is running on port:"+PORT)
