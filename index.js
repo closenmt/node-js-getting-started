df695034e7fed8b6708fe8f26abc6beba920ac53
closenmt <closenmt@gmail.com>
2021-12-07 13:38:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const httpProxy=require('http-proxy')
+httpProxy.createServer({
+  target: {
+    host: 'https://ooxxz.oschina.link',
+    port: 443
+  },
+  ssl: {
+    key: fs.readFileSync('valid-ssl-key.pem', 'utf8'),
+    cert: fs.readFileSync('valid-ssl-cert.pem', 'utf8')
+  }
+}).listen(PORT);
